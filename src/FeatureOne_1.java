f59d94d54f798bb6dfe0e936dde5b15d74f8c49f
Serge Rozkov <rozkov_ss@mail.ru>
2022-12-02 08:09:26 +0300

    commit changes

---
+++ src/FeatureOne_1.java
@@ -0,0 +1,12 @@
+public class FeatureOne_1 {
+
+    private int id;
+
+    public FeatureOne_1(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
 
+        FeatureOne_1 f1 = new FeatureOne_1(1);
+
     }
 }
